8341b63858336a2fbadfe55d66c94ae314cfb10c
fuyuchao <fuyuchao@9fbank.com.cn>
2019-05-22 10:29:36 +0800

    初始化

---
+++ derive_excel/src/main/java/com/fycstart/DeriveExcelApplication.java
@@ -0,0 +1,14 @@
+package com.fycstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@MapperScan("com.fycstart.mapper")
+public class DeriveExcelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeriveExcelApplication.class, args);
+    }
+
+}
---
+++ searchrome/src/main/java/com/fycstart/search_room/search_room/SearchRoomApplication.java
@@ -0,0 +1,13 @@
+package com.fycstart.search_room.search_room;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchRoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchRoomApplication.class, args);
+    }
+
+}
